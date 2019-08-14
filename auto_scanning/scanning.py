a27acaf0fddb675a6539e813eb381ca8cedc5ea7
Suvrajit karmaker <38050162+suvrajitkarmaker@users.noreply.github.com>
2019-08-15 03:51:20 +0600

    Create scanning.py

---
+++ auto_scanning/scanning.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pyautogui
+import time
+
+
+# In[4]:
+
+
+pyautogui.position()
+
+
+# In[5]:
+
+
+pyautogui.position()
+
+
+# In[7]:
+
+
+for i in range(1,51,1):
+    pyautogui.click(808,377)
+    time.sleep(23)
+    pyautogui.click(1591,0)
+
+
+# In[ ]:
+
+
